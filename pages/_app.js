b3542f8ccf84356000478f170b7f84ab5258b9f1
jawad-elhajjami <jawadelhajjamiofficiel@gmail.com>
2025-03-23 18:25:31 +0000

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from "react";
 import { Footer } from "@/components";
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react"
+
 
 
 export default function App({ Component, pageProps }) {
